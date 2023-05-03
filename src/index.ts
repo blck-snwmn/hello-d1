d304e228f579c2b6fc7d1719a5618c8c906a9096
blck-snwmn <whitesnowmancreator@gmail.com>
2023-05-04 00:48:33 +0900

    Exec query to d1

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,11 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
+	DB: D1Database
+}
+
+type memo = {
+	content: string,
+	user: string,
+	tag: string,
 }
 
 export default {
@@ -28,6 +14,31 @@ export default {
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
-		return new Response("Hello World!");
+		switch (request.method) {
+			case "GET":
+				return list(env);
+			case "POST":
+				const { content, user, tag } = await request.json() as memo;
+				return postMemo(env, content, user, tag);
+			default:
+				return new Response("Method not allowed", { status: 405 });
+		}
+
 	},
 };
+
+async function list(env: Env): Promise<Response> {
+	const { results } = await env.DB.prepare("SELECT * FROM memos").all();
+	return Response.json(results);
+}
+
+async function postMemo(env: Env, content: string, user: string, tag: string) {
+	const { success } = await env.DB.
+		prepare(`INSERT INTO memos (content, user, tag) VALUES (?, ?, ?)`).
+		bind(content, user, tag).
+		run();
+	if (success) {
+		return new Response("OK", { status: 200 });
+	}
+	return new Response("Internal Server Error", { status: 500 });
+}
